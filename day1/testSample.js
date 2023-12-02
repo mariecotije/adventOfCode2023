d5a534dc335241f49d18daf559dff96c094c79c0
mariecotije <kkmarushka@gmail.com>
2023-12-03 00:44:26 +0100

    solved day 2 puzzles

---
+++ day1/testSample.js
@@ -0,0 +1,25 @@
+const sample = [
+    "two1nine",
+    "eightwothree",
+    "abcone2threexyz",
+    "xtwone3four",
+    "4nineeightseven2",
+    "zoneight234",
+    "7pqrstsixteen"
+];
+
+// const inputString = "two1nine";
+
+// let index = inputString.indexOf("two");
+// console.log(index);
+let digits = [];
+
+sample.forEach(element => {
+    let one = element.indexOf("one");
+    let two = element.indexOf("two");
+
+    digits.push()
+
+});
+
+console.log("First digits: ", digits);
